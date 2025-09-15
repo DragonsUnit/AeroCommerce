126bd62d44f716b7baae910b14535dc77c77963d
DragonUnit <dragonspowerunit@gmail.com>
2025-09-15 14:29:38 +0530

    Initial commit

--- middlewares/authAdmin.js
+++ middlewares/authAdmin.js
@@ -1,18 +1,18 @@
 import { clerkClient } from "@clerk/nextjs/server"
 
-
 const authAdmin = async (userId) => {
-    try {
-        if(!userId) return false
+  try {
+    if (!userId) return false
 
-        const client = await clerkClient()
-        const user = await client.users.getUser(userId)
+    const user = await clerkClient.users.getUser(userId)
+    const email = user.emailAddresses?.[0]?.emailAddress
+    if (!email) return false
 
-        return process.env.ADMIN_EMAIL.split(',').includes(user.emailAddresses[0].emailAddress)
-    } catch (error) {
-        console.error(error)
-        return false
-    }
+    return process.env.ADMIN_EMAIL?.split(',').includes(email) ?? false
+  } catch (error) {
+    console.error('AuthAdmin Error:', error)
+    return false
+  }
 }
 
 export default authAdmin
